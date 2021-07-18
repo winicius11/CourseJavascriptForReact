18443358d32279b738ecdd17613ea5a4031365d6
Winicius Moreira <winicius.pousada@gmail.com>
2021-07-18 16:17:54 -0300

    :construction: Create object BoardGame without CSS

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 
-import CardGame from './src/components/CardGame';
+import PlayerName from './src/components/PlayerName';
+import BoardGame from './src/objects/BoardGame';
 
 const $root = document.querySelector('#root');
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(6);
+const $htmlPlayer1 = PlayerName(1);
+const $htmlPlayer2 = PlayerName(2);
 
-$root.insertAdjacentHTML('beforeend', $htmlCardGame);
+$root.insertAdjacentHTML('beforeend', $htmlPlayer1);
+$root.insertAdjacentHTML('beforeend', $htmlPlayer2);
+
+$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
+// for (let i = 1; i <= 6; i++) {
+    // $root.insertAdjacentHTML('beforeend', $htmlCardGame);
+// }
+
+
---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+function PlayerName(name) {
+    return `
+      <p>Player 1</p>
+    `
+}
+
+export default PlayerName;
