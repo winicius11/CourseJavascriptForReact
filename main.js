260e8ffb933eda29d080900de1187e74c2d99521
Winicius Moreira <winicius.pousada@gmail.com>
2021-07-18 18:34:42 -0300

    :lipstick: Create ScoreBoard object
    git push

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 
-import PlayerName from './src/components/PlayerName';
 import BoardGame from './src/objects/BoardGame';
+import ScoreBoard from './src/objects/ScoreBoard';
 
 const $root = document.querySelector('#root');
 
 $root.insertAdjacentHTML(
   'beforeend', 
   `
-   ${PlayerName('Player1')}
-   ${PlayerName('Player2')}
-   ${BoardGame(6)}
+  ${ScoreBoard()}    
+  ${BoardGame(6)}
   `
 );
