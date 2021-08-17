fe1799eb77e58991470aa68db9d231148be4a59f
NOTE-WINICIUS\Winicius <winicius.pousada@seventh.com.br>
2021-08-16 22:59:09 -0300

    Adding click event on CardFrontBack

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,8 +3,17 @@ import './style.css';
 import CardGame from "../CardGame";
 
 function CardFrontBack(){
-    return  /*html*/`
-      <article class="card-front-back">
+
+  window.cardFrontBack = {}
+  window.cardFrontBack.handleClick = (event) => {
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest('.card-front-back');
+
+    $cardFrontBack.classList.toggle('-active');
+  }  
+
+  return  /*html*/`
+      <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
         <div class="card -front">
           ${CardGame()}
         </div>
