f3c216b75d018eb7c94f4837873d8bd8ed191331
Winicius Moreira <winicius.pousada@gmail.com>
2021-08-17 23:03:34 -0300

    :construction: Created VsPlayer component

---
+++ src/components/VsPlayer/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function VsPlayer(content = "vs") {
+
+    return /*html*/ `
+      <span class="vs-player">
+        ${content}
+      </span>
+    `
+}
+
+export default VsPlayer;
